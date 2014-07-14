0841fbd9950e12cef75bc6394b02ccf38302b476
Jose Dimas <jrdv009@neo.tamu.edu>
2014-07-14 08:56:03 -0500

    Plotters

---
+++ GEMValidation/scripts/Custom Plots/Checked PT_ETA.py
@@ -0,0 +1,267 @@
+import ROOT
+import sys
+
+#_______________________________________________________________________________
+def getRootDirectory(input_dir2, file_name2, dir2_name = "GEMCSCTriggerEfficiency"):
+    """Get the ROOT directory from the GEMCSCTriggerEfficiency analyzer. Normally,
+    the directory should be called GEMCSCTriggerEfficiency. You should check it anyway"""
+
+    ### get the ROOT file
+    file = ROOT.TFile.Open(input_dir2 + file_name2)
+    if not file:
+        sys.exit('Input ROOT file %s is missing.' %(file_name2))
+
+    ## get the ROOT directory
+    dir = file.Get(dir2_name)
+    if not dir:
+        sys.exit('Directory %s does not exist.' %(dir2_name))
+
+    return dir
+
+#_______________________________________________________________________________
+def getH(dir, histo_name):
+  """Get the histogram from a directory"""
+  histo = dir.Get("%s;1"%(histo_name))
+  if not histo:
+    print "No such histogram: ", histo_name
+  return histo
+
+#_______________________________________________________________________________
+def myRebin(h, n):
+  """Custom rebin function"""
+  nb = h.GetNbinsX()
+  entr = h.GetEntries()
+  bin0 = h.GetBinContent(0)
+  binN1 = h.GetBinContent(nb+1)
+  if (nb % n):
+    binN1 += h.Integral(nb - nb%n + 1, nb)
+  h.Rebin(n)
+  nb = h.GetNbinsX()
+  h.SetBinContent(0, bin0)
+  h.SetBinContent(nb+1, binN1)
+  h.SetEntries(entr)
+
+#______________________________________________________________________________
+
+def setEffHisto2(num_name, den_name, dir, nrebin, lcolor, lstyle, lwidth, mcolor, mstyle,
+                htitle, xtitle, ytitle, x_range, y_range):
+    hd = getH(dir, den_name)
+    hn = getH(dir, num_name)
+    hd.Sumw2()
+    hn.Sumw2()
+    myRebin(hd, nrebin)
+    myRebin(hn, nrebin)
+    heff = hn.Clone(num_name+"_eff")
+    hd.Sumw2()
+    heff.Sumw2()
+    #heff.Divide(heff,hd)
+    heff.SetLineColor(lcolor)
+    heff.SetLineStyle(lstyle)
+    heff.SetMarkerColor(mcolor)
+    heff.SetMarkerStyle(mstyle)
+    heff.SetLineWidth(lwidth)
+    heff.SetTitle(htitle)
+    heff.GetXaxis().SetTitle(xtitle)
+    heff.GetYaxis().SetTitle(ytitle)
+    heff.GetXaxis().SetRangeUser(x_range[0],x_range[1])
+    heff.GetYaxis().SetRangeUser(y_range[0],y_range[1])
+    heff.GetXaxis().SetTitleSize(0.05)
+    heff.GetXaxis().SetTitleOffset(1.)
+    heff.GetYaxis().SetTitleSize(0.05)
+    heff.GetYaxis().SetTitleOffset(1.)
+    heff.GetXaxis().SetLabelOffset(0.015)
+    heff.GetYaxis().SetLabelOffset(0.015)
+    heff.GetXaxis().SetLabelSize(0.05)
+    heff.GetYaxis().SetLabelSize(0.05)
+    return heff
+
+#_____________________________________________________________________________________
+def getEff(file,dir,den,num):
+    f = ROOT.TFile(file)
+    t = f.Get(dir)
+    h1 = ROOT.TH1F("h1","h1",40,1.5,2.5)
+    t.Draw("eta >> h1",den)
+    h2 = ROOT.TH1F("h2","h2",40,1.5,2.5)
+    t.Draw("eta >> h2",num)
+    e = ROOT.TEfficiency(h2,h1)
+    return e
+
+##################3 End of functions set up #################
+
+
+def TreeBasedPT():
+    c1 = ROOT.TCanvas()
+    c1.SetGridx()
+    c1.SetGridy()
+    c1.SetTickx()
+    c1.SetTicky()
+    
+    f2 = ROOT.TFile(file)
+    t2 = f2.Get("GEMCSCAnalyzer/trk_eff_ME11")
+    e2 = ROOT.TH1F("e2","e2",40,0,50)
+    t2.Draw("pt >> e2",num)
+    eb = ROOT.TH1F("eb","eb",40,0,50)
+    t2.Draw("pt >> eb",den)
+
+    e2.SetLineColor(ROOT.kRed)
+    e2.SetLineWidth(2)
+    e2.SetLineStyle(1)
+
+    e2.SetLineWidth(3)
+    eb.SetLineWidth(3)
+    
+    b1 = ROOT.TH1F("b1","b1",40,0,60)
+    b1.GetYaxis().SetRangeUser(0,5000)
+    b1.GetYaxis().SetTitleOffset(1.2)
+    b1.GetYaxis().SetNdivisions()
+    #b1.GetYaxis().SetTitle("LCT reconstruction efficiency")
+    b1.GetXaxis().SetTitle("PT")
+    b1.SetTitle("Number of Samples" )
+    b1.SetStats(0)
+    b1.Draw()
+
+    eb.Draw("same")
+    e2.Draw("same")
+
+    legend = ROOT.TLegend(0.25,0.13,0.72,0.42)
+    legend.SetFillColor(ROOT.kWhite)
+    legend.SetHeader("PU140 and 10<Pt<50")
+    legend.AddEntry(e2,"TMB Baseline 2: num","l")
+    legend.AddEntry(eb,"TMB Baseline 2: den","l")
+    legend.Draw("same")
+
+    c1.SaveAs("Tree_Based_PT.png")
+
+def TreeBasedETa():
+    c1 = ROOT.TCanvas()
+    c1.SetGridx()
+    c1.SetGridy()
+    c1.SetTickx()
+    c1.SetTicky()
+    
+    f2 = ROOT.TFile(file)
+    t2 = f2.Get("GEMCSCAnalyzer/trk_eff_ME11")
+    e2 = ROOT.TH1F("e2","e2",40,1.5,2.5)
+    t2.Draw("eta >> e2",num)
+    eb = ROOT.TH1F("eb","eb",40,1.5,2.5)
+    t2.Draw("eta >> eb",den)
+
+    e2.SetLineColor(ROOT.kRed)
+    e2.SetLineWidth(2)
+    e2.SetLineStyle(1)
+
+    e2.SetLineWidth(3)
+    eb.SetLineWidth(3)
+    
+    b1 = ROOT.TH1F("b1","b1",40,1.4,2.5)
+    b1.GetYaxis().SetRangeUser(0,2500)
+    b1.GetYaxis().SetTitleOffset(1.2)
+    b1.GetYaxis().SetNdivisions()
+    #b1.GetYaxis().SetTitle("LCT reconstruction efficiency")
+    b1.GetXaxis().SetTitle("#eta of SimTrack muon")
+    b1.SetTitle("Number of Samples" )
+    b1.SetStats(0)
+    b1.Draw()
+
+    eb.Draw("same")
+    e2.Draw("same")
+
+    legend = ROOT.TLegend(0.25,0.13,0.72,0.42)
+    legend.SetFillColor(ROOT.kWhite)
+    legend.SetHeader("PU140 and 10<Pt<50")
+    legend.AddEntry(e2,"TMB Baseline 2: num","l")
+    legend.AddEntry(eb,"TMB Baseline 2: den","l")
+    legend.Draw("same")
+
+    c1.SaveAs("Tree_Based_Eta.png")
+
+def HistBasedEta():
+    input_dir=""
+    d1 = getRootDirectory(input_dir, f1)
+    etareb = 1
+    yrange = [0,2500]
+    xrange = [1.4,2.5]    
+    xTitle = "Sim track #eta"
+    yTitle = "Number of Samples"
+    topTitle = "Number of Samples, eta"
+    title = "%s;%s;%s"%(topTitle,xTitle,yTitle)
+
+    h1 = setEffHisto2("h_eta_me1_after_lct_okAlctClct","h_eta_me1_initial",d1, etareb, ROOT.kRed+1, 1, 2, ROOT.kRed, 4, topTitle,xTitle,yTitle,xrange,yrange)
+    h7 = setEffHisto2("h_eta_me1_initial","h_eta_me1_after_lct_okAlctClct",d1, etareb, ROOT.kBlue, 1, 2, ROOT.kRed, 4, topTitle,xTitle,yTitle,xrange,yrange)
+
+    c = ROOT.TCanvas("h_eta_me1_after_lct_okAlctClct","h_eta_me1_initial",1000,600 )     
+    c.cd()
+    c.SetGridx()
+    c.SetGridy()
+    c.SetTickx()
+    c.SetTicky()
+    h1.SetStats(0)
+    h7.SetStats(0)
+    h1.Draw("hist")
+    h7.Draw("same hist")
+
+    leg = ROOT.TLegend(0.18,0.18,0.78,0.44,"","brNDC")
+    leg.SetMargin(0.15)
+    leg.SetBorderSize(0)
+    leg.SetFillStyle(0)
+    leg.AddEntry(h1,"TMB Baseline 2 PU140 num","l")
+    leg.AddEntry(h7,"TMB Baseline 2 PU140 den","l")
+    leg.Draw()
+    #tex = drawPULabel()
+    c.SaveAs("Hist_Based_Eta.png")
+
+def HistBasedPT():
+    input_dir=""
+    d1 = getRootDirectory(input_dir, f1)
+    etareb = 1
+    yrange = [0,5000]
+    xrange = [0,50]    
+    xTitle = "Sim track #eta"
+    yTitle = "Number of Samples"
+    topTitle = "Number of Samples, eta"
+    title = "%s;%s;%s"%(topTitle,xTitle,yTitle)
+
+    h1 = setEffHisto2("h_pt_me1_initial","h_pt_initial0",d1, etareb, ROOT.kRed+1, 1, 2, ROOT.kRed, 4, topTitle,xTitle,yTitle,xrange,yrange)
+    h7 = setEffHisto2("h_pt_initial0","h_pt_me1_initial",d1, etareb, ROOT.kBlue, 1, 2, ROOT.kRed, 4, topTitle,xTitle,yTitle,xrange,yrange)
+
+    c = ROOT.TCanvas("h_pt_me1_initial","h_pt_me1_initial",1000,600 )     
+    c.cd()
+    c.SetGridx()
+    c.SetGridy()
+    c.SetTickx()
+    c.SetTicky()
+    b1 = ROOT.TH1F("b1","b1",40,0,60)
+    b1.GetYaxis().SetRangeUser(0,5000)
+    b1.GetYaxis().SetTitleOffset(1.2)
+    b1.GetYaxis().SetNdivisions()
+    #b1.GetYaxis().SetTitle("LCT reconstruction efficiency")
+    b1.GetXaxis().SetTitle("#eta of SimTrack muon")
+    b1.SetTitle("Number of Samples" )
+    b1.SetStats(0)
+    b1.Draw()
+    h1.Draw("hist same")
+    h7.Draw("same hist")
+
+    leg = ROOT.TLegend(0.18,0.18,0.88,0.58,"","brNDC")
+    leg.SetMargin(0.15)
+    leg.SetBorderSize(0)
+    leg.SetFillStyle(0)
+    leg.AddEntry(h1,"TMB Baseline 2 PU140 num","l")
+    leg.AddEntry(h7,"TMB Baseline 2 PU140 den","l")
+    leg.Draw()
+    #tex = drawPULabel()
+    c.SaveAs("Hist_Based_PT.png")
+
+#### Main 
+
+f1="hp_dimu_CMSSW_6_2_0_SLHC7_upgrade2019_w3PU140_TMB_Baseline_2_eff.root"
+file= "hp_CMSSW14_usingL1SLHC7_PU140_TMB_Baseline_2_eff.root"
+
+tree = "GEMCSCAnalyzer/trk_eff_ME11"
+den = "has_csc_sh>0 && pt>10"
+num = "has_csc_sh>0 && has_lct>0 && pt>10"
+
+TreeBasedPT()
+TreeBasedETa()
+HistBasedEta()
+HistBasedPT()
---
+++ GEMValidation/scripts/Custom Plots/Whole LCT.py
@@ -0,0 +1,192 @@
+import ROOT
+
+c1 = ROOT.TCanvas()
+c1.SetGridx()
+c1.SetGridy()
+c1.SetTickx()
+c1.SetTicky()
+
+
+tree = "GEMCSCAnalyzer/trk_eff_ME11"
+den = "has_csc_sh>0 && pt>10"
+num = "has_csc_sh>0 && has_lct>0 && pt>10"
+'''
+f1 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_TMB_Baseline_1_eff.root")
+t1 = f1.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e1 = ROOT.TH1F("e1","e1",40,1.5,2.5)
+t1.Draw("eta >> e1",num)
+ea = ROOT.TH1F("ea","ea",40,1.5,2.5)
+t1.Draw("eta >> ea",den)
+e1.Divide(ea)
+'''
+
+f2 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_TMB_Baseline_2_eff.root")
+t2 = f2.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e2 = ROOT.TH1F("e2","e2",40,1.5,2.5)
+t2.Draw("eta >> e2",num)
+eb = ROOT.TH1F("eb","eb",40,1.5,2.5)
+t2.Draw("eta >> eb",den)
+e2.Divide(eb)
+
+
+f3 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_matchTrigWindowSize_eff.root")
+t3 = f3.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e3 = ROOT.TH1F("e3","e3",40,1.5,2.5)
+t3.Draw("eta >> e3",num)
+ec = ROOT.TH1F("ec","ec",40,1.5,2.5)
+t3.Draw("eta >> ec",den)
+e3.Divide(ec)
+
+f4 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_alctUseCorrectedBx_eff.root")
+t4 = f4.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e4 = ROOT.TH1F("e4","e4",40,1.5,2.5)
+t4.Draw("eta >> e4",num)
+ed = ROOT.TH1F("ed","ed",40,1.5,2.5)
+t4.Draw("eta >> ed",den)
+e4.Divide(ed)
+
+
+f5 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_clctUseCorrectedBx_eff.root")
+t5 = f5.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e5 = ROOT.TH1F("e5","e5",40,1.5,2.5)
+t5.Draw("eta >> e5",num)
+ee = ROOT.TH1F("ee","ee",40,1.5,2.5)
+t5.Draw("eta >> ee",den)
+e5.Divide(ee)
+
+f6 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_clctToAlct_eff.root")
+t6 = f6.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e6 = ROOT.TH1F("e6","e6",40,1.5,2.5)
+t6.Draw("eta >> e6",num)
+ef = ROOT.TH1F("ef","ef",40,1.5,2.5)
+t6.Draw("eta >> ef",den)
+e6.Divide(ef)
+
+
+f7 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_tmbDropUsedClcts_matchEarliestClctME11Only_eff.root")
+t7 = f7.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e7 = ROOT.TH1F("e7","e7",40,1.5,2.5)
+t7.Draw("eta >> e7",num)
+eg = ROOT.TH1F("eg","eg",40,1.5,2.5)
+t7.Draw("eta >> eg",den)
+e7.Divide(eg)
+
+
+f8 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_tmbCrossBxAlgorithm_eff.root")
+t8 = f8.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e8 = ROOT.TH1F("e8","e8",40,1.5,2.5)
+t8.Draw("eta >> e8",num)
+eh = ROOT.TH1F("eh","eh",40,1.5,2.5)
+t8.Draw("eta >> eh",den)
+e8.Divide(eh)
+
+
+
+f9 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_tmbReadoutEarliest2_eff.root")
+t9 = f9.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e9 = ROOT.TH1F("e9","e9",40,1.5,2.5)
+t9.Draw("eta >> e9",num)
+ei = ROOT.TH1F("ei","ei",40,1.5,2.5)
+t9.Draw("eta >> ei",den)
+e9.Divide(ei)
+
+
+
+
+f10 = ROOT.TFile("hp_CMSSW14_usingL1SLHC7_PU140_SLHC_eff.root")
+t10 = f10.Get("GEMCSCAnalyzer/trk_eff_ME11")
+e10 = ROOT.TH1F("e10","e10",40,1.5,2.5)
+t10.Draw("eta >> e10",num)
+ej = ROOT.TH1F("ej","ej",40,1.5,2.5)
+t10.Draw("eta >> ej",den)
+e10.Divide(ej)
+
+
+#e1.SetLineColor(ROOT.kBlack)
+#e1.SetLineWidth(2)
+#e1.SetLineStyle(2)
+
+
+e2.SetLineColor(ROOT.kRed)
+e2.SetLineWidth(2)
+e2.SetLineStyle(1)
+
+e3.SetLineColor(ROOT.kGreen+2)
+e3.SetLineWidth(2)
+e3.SetMarkerStyle(4)
+e3.SetMarkerColor(ROOT.kGreen+2)
+
+e4.SetLineColor(ROOT.kYellow)
+e4.SetLineWidth(2)
+e4.SetMarkerStyle(21)
+e4.SetMarkerColor(ROOT.kGreen+2)
+
+e5.SetLineColor(ROOT.kViolet-2)
+e5.SetLineWidth(4)
+e5.SetMarkerStyle(22)
+e5.SetMarkerColor(ROOT.kGreen+2)
+
+e6.SetLineColor(ROOT.kYellow-1)
+e6.SetLineWidth(2)
+e6.SetMarkerStyle(23)
+e6.SetMarkerColor(ROOT.kGreen+2)
+
+e7.SetLineColor(ROOT.kPink+1)
+e7.SetLineWidth(2)
+e7.SetMarkerStyle(25)
+e7.SetMarkerColor(ROOT.kGreen+2)
+
+e8.SetLineColor(ROOT.kOrange+2)
+e8.SetLineWidth(2)
+e8.SetMarkerStyle(26)
+e8.SetMarkerColor(ROOT.kGreen+2)
+
+e9.SetLineColor(ROOT.kGreen-2)
+e9.SetLineWidth(4)
+e9.SetMarkerStyle(20)
+e9.SetMarkerColor(ROOT.kGreen+2)
+
+e10.SetLineColor(ROOT.kBlue)
+e10.SetLineWidth(2)
+e10.SetLineStyle(2)               
+
+b1 = ROOT.TH1F("b1","b1",40,1.4,2.5)
+b1.GetYaxis().SetRangeUser(0.5,1.01)
+b1.GetYaxis().SetTitleOffset(1.2)
+b1.GetYaxis().SetNdivisions(520)
+b1.GetYaxis().SetTitle("LCT reconstruction efficiency")
+b1.GetXaxis().SetTitle("#eta of simulated muon track")
+b1.SetTitle("Tree Based")
+b1.SetStats(0)
+
+b1.Draw()
+e2.Draw("same")
+e3.Draw("same P")
+e4.Draw("same P")
+e5.Draw("same P")
+e6.Draw("same p")
+e7.Draw("same p")
+e8.Draw("same p")
+e9.Draw("same p")
+e10.Draw("same")
+
+legend = ROOT.TLegend(0.17,0.13,0.72,0.52)
+legend.SetFillColor(ROOT.kWhite)
+#legend.SetBorderSize(0)
+#legend.SetFillStyle(0)
+legend.SetHeader("PU140 and 2<Pt<50")
+#legend.AddEntry(e1,"TMB Baseline 1","l")
+legend.AddEntry(e2,"TMB Baseline 2 num","l")
+#legend.AddEntry(eb,"TMB Baseline 2 den","l")
+legend.AddEntry(e3,"matchTrigWindowSize: 7BX to 3BX","p")
+legend.AddEntry(e4,"alctUseCorrectedBx: False to True","p")
+legend.AddEntry(e5,"clctUseCorrectedBx: False to True","p")
+legend.AddEntry(e6,"clctToAlct: True to False","p")
+legend.AddEntry(e7,"tmbDropUsedClcts && matchEarliestClctME11Only: False to True","p")
+legend.AddEntry(e8,"tmbCrossBxAlgorithm: 0 to 1","p")
+legend.AddEntry(e9,"tmbReadoutEarliest2: True to False","p")
+legend.AddEntry(e10,"TMB SLHC","l")
+legend.Draw("same")
+
+c1.SaveAs("2LCT_reco_eff_PU140.pdf")
+c1.SaveAs("2LCT_reco_eff_PU140.png")
